fc579b65984ee9714f8ccb62688e1c8b17201736
Vishal Mishra <vishalmishra090@gmail.com>
2022-02-16 14:56:05 +0530

    update app with router

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import { Suspense } from "react";
+import { CircularProgress } from '@mui/material';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Routes from "./routes";
+import Views from "./views";
+
+const App = () => {
+    return (
+      <>
+        <Views>
+          <Suspense fallback={<CircularProgress />}>
+            <Routes />
+          </Suspense>
+        </Views>
+      </>
+    );
 }
 
 export default App;
