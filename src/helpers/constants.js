784802124b61754070addf824204db11c5cc2abb
Vishal Mishra <vishalmishra090@gmail.com>
2022-02-16 14:40:31 +0530

    add constants file

---
+++ src/helpers/constants.js
@@ -0,0 +1,7 @@
+export const TMDB_IMG_URL = {
+    medium: "https://image.tmdb.org/t/p/w200",
+};
+
+export const PLACEHOLDER_IMG_URL = {
+    medium: "https://dummyimage.com/200x300/d9d9d9/382a63.png&text=No+Image"
+};
